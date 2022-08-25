c3137988bc7776ec06ddc760179e3a09db447cb9
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 19:03:56 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Color");
+        int V = in.nextInt();
+        switch (V) {
+            case 380-450:
+                System.out.println("Violet");}
+        System.out.println("Violet");
+        int B = in.nextInt();
+        switch (B) {
+            case 450-495:
+                System.out.println("Blue");}
+        System.out.println("Blue");
+        int G = in.nextInt();
+        switch (G) {
+            case 495-570:
+                System.out.println("Green");}
+        System.out.println("Green");
+        int Y = in.nextInt();
+        switch (Y) {
+            case 570-590:
+                System.out.println("Yellow");}
+        System.out.println("Yellow");
+        int O = in.nextInt();
+        switch (O) {
+            case 590-620:
+                System.out.println("Orange");}
+        System.out.println("Orange");
+        int R = in.nextInt();
+        switch (R) {
+            case 620-750:
+                System.out.println("Red");}
+        System.out.println("Red");
+        
+    }
+}
